d7f97240ea52a0f5700f8b68a892e13c230b93ea
Dirk Groeneveld <dirkg@allenai.org>
2015-12-17 13:38:54 -0800

    Proper access annotation for year

--- src/main/java/org/allenai/scienceparse/ExtractedMetadata.java
+++ src/main/java/org/allenai/scienceparse/ExtractedMetadata.java
@@ -35,7 +35,7 @@ public class ExtractedMetadata {
   public List<String> raw; //the full paper text
   public List<String> rawReferences; //approximately the references text
   public Pair<List<BibRecord>, List<CitationRecord>> references; //references, with char offsets relative to raw
-  int year;
+  public int year;
 
   /**
    * Constructs ExtractedMetadata from given text and labels
