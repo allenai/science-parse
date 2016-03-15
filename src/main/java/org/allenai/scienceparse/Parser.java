9bff7b0a53c284555cce3f92e2e97116517240ef
Dirk Groeneveld <dirkg@allenai.org>
2016-03-15 09:28:25 -0700

    Use model trained with the new cleanup step

--- src/main/java/org/allenai/scienceparse/Parser.java
+++ src/main/java/org/allenai/scienceparse/Parser.java
@@ -77,7 +77,7 @@ public class Parser {
 
   private static final Datastore datastore = Datastore.apply();
   public static Path getDefaultProductionModel() {
-    return datastore.filePath("org.allenai.scienceparse", "productionModel.dat", 4);
+    return datastore.filePath("org.allenai.scienceparse", "productionModel-ce5b11.dat", 1);
   }
   public static Path getDefaultGazetteer() {
     return datastore.filePath("org.allenai.scienceparse", "gazetteer-1m.json", 1);
