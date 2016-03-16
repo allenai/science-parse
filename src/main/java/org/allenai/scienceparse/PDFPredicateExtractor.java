a1ecef3ec822602ff9bae2e38de2eb86ff2ba48c
Dirk Groeneveld <dirkg@allenai.org>
2016-03-15 17:33:15 -0700

    Adds the "and" feature

--- src/main/java/org/allenai/scienceparse/PDFPredicateExtractor.java
+++ src/main/java/org/allenai/scienceparse/PDFPredicateExtractor.java
@@ -249,6 +249,9 @@ public class PDFPredicateExtractor implements CRFPredicateExtractor<PaperToken, 
         final String token = StringUtils.normalize(elems.get(i).getPdfToken().token);
 		m.put("%t=" + token, 1.0);
 
+        if(token.equals("and") || token.equals(","))
+          m.put("%and", 1.0);
+
         // add trigram features
         final String trigramSourceToken = token + "$";
         for(int j = 0; j <= trigramSourceToken.length() - 3; ++j) {
