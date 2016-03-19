8d0596a69da0a6beb4de633260761188cda40fec
Dirk Groeneveld <dirkg@allenai.org>
2016-03-19 13:47:50 -0700

    Ablate the dash feature instead of the token feature

--- src/main/java/org/allenai/scienceparse/PDFPredicateExtractor.java
+++ src/main/java/org/allenai/scienceparse/PDFPredicateExtractor.java
@@ -261,13 +261,13 @@ public class PDFPredicateExtractor implements CRFPredicateExtractor<PaperToken, 
 
         // add the token itself as a feature
         final String token = StringUtils.normalize(elems.get(i).getPdfToken().token);
-		//m.put("%t=" + token, 1.0);
+		m.put("%t=" + token, 1.0);
 
         if(token.equals("and") || token.equals(","))
           m.put("%and", 1.0);
 
-        if(token.equals("-"))
-          m.put("%dash", 1.0);
+        //if(token.equals("-"))
+        //  m.put("%dash", 1.0);
 
         // add trigram features
         final String trigramSourceToken = token + "$";
