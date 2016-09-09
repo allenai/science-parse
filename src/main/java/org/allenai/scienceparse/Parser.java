96bc79de37a712631b64a4b7c662a57dc0826116
dcdowney <ddowney@eecs.northwestern.edu>
2016-09-09 17:53:47 -0500

    merge with master, patches

--- src/main/java/org/allenai/scienceparse/Parser.java
+++ src/main/java/org/allenai/scienceparse/Parser.java
@@ -44,6 +44,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.Normalizer;
