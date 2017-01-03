0a9532289ecb4d47d08d319c36d7c7eb619c35c1
Dirk Groeneveld <dirkg@allenai.org>
2017-01-03 10:48:54 -0800

    Proper JSON formatting

--- src/main/scala/org/allenai/scienceparse/GazetteerFromPMC.scala
+++ src/main/scala/org/allenai/scienceparse/GazetteerFromPMC.scala
@@ -2,19 +2,23 @@ package org.allenai.scienceparse
 
 import org.allenai.common.ParIterator._
 import scala.concurrent.ExecutionContext.Implicits.global
+import spray.json._
 
 object GazetteerFromPMC extends App {
+  case class GazetteerEntry(id: String, title: String, authors: Seq[String], year: Int)
+  import DefaultJsonProtocol._
+  implicit val gazetteerEntryFormat = jsonFormat4(GazetteerEntry.apply)
+
   // We use the first 1k of this for testing, so let's drop 10k just to be sure.
   val labeledDataNotUsedForTesting = LabeledDataFromPMC.get.drop(10000)
 
   labeledDataNotUsedForTesting.parMap { ld =>
     (ld.title, ld.authors, ld.year) match {
-      case (Some(title), Some(authors), Some(year)) => Some((ld.paperId, title, authors.map(_.name), year))
+      case (Some(title), Some(authors), Some(year)) =>
+        Some(GazetteerEntry(ld.paperId, title, authors.map(_.name), year))
       case _ => None
     }
-  }.flatten.take(1000000).foreach { case (paperId, title, authors, year) =>
-    def escape(s: String) = s.replace("\"", "\\\"")
-    val authorsString = "[\"" + authors.map(escape).mkString("\",\"") + "\"]"
-    println(s"""{"id":"$paperId","title":"${escape(title)}","authors":$authorsString,"year":$year},""")
+  }.flatten.take(1000000).foreach { entry =>
+    println(entry.toJson)
   }
 }
