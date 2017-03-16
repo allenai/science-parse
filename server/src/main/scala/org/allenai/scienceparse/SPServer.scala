6d3b96d6288111c288518ffe114d4ef32ada7100
Dirk Groeneveld <dirkg@allenai.org>
2017-03-16 12:31:41 -0700

    Adds ability to turn off the feedback mechanism

--- server/src/main/scala/org/allenai/scienceparse/SPServer.scala
+++ server/src/main/scala/org/allenai/scienceparse/SPServer.scala
@@ -1,5 +1,6 @@
 package org.allenai.scienceparse
 
+import java.awt.print.Book
 import java.io.{File, StringWriter, InputStream, ByteArrayInputStream}
 import java.security.{DigestInputStream, MessageDigest}
 import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
@@ -29,7 +30,8 @@ object SPServer extends Logging {
       modelFile: Option[File] = None,
       bibModelFile: Option[File] = None,
       gazetteerFile: Option[File] = None,
-      paperDirectory: Option[File] = None
+      paperDirectory: Option[File] = None,
+      enableFeedback: Boolean = true
     )
 
     val parser = new OptionParser[Config](this.getClass.getSimpleName) {
@@ -50,6 +52,10 @@ object SPServer extends Logging {
       opt[File]('p', "paperDirectory") action { (p, c) =>
         c.copy(paperDirectory = Some(p))
       } text "Specifies a directory with papers in them. If this is not specified, or a paper can't be found in the directory, we fall back to getting the paper from the bucket."
+
+      opt[Unit]("disableFeedback") action { (_, c) =>
+        c.copy(enableFeedback = false)
+      } text "Disabled the feedback mechanism"
 
       help("help") text "Prints help text"
     }
@@ -76,7 +82,7 @@ object SPServer extends Logging {
       }
 
       val server = new Server(8080)
-      server.setHandler(new SPServer(paperSource, scienceParser))
+      server.setHandler(new SPServer(paperSource, scienceParser, config.enableFeedback))
       server.start()
       server.join()
     }
@@ -87,7 +93,8 @@ object SPServer extends Logging {
 
 class SPServer(
   private val paperSource: PaperSource,
-  private val scienceParser: Parser
+  private val scienceParser: Parser,
+  enableFeedback: Boolean = true
 ) extends AbstractHandler with Logging {
 
   private val jsonMapper = new ObjectMapper() with ScalaObjectMapper
@@ -201,6 +208,14 @@ class SPServer(
     }
   }
 
+  private val feedbackStore = if(enableFeedback) Some(FeedbackStore) else None
+
+  private val feedbackRoutes = Seq(
+    StringRoute("/v1/corrections")(correctionsGetAll),
+    RegexRoute("^/v1/corrections/([a-f0-9]{40})$".r("paperId"), "GET")(correctionsGet),
+    RegexRoute("^/v1/corrections/([a-f0-9]{40})$".r("paperId"), "PUT")(correctionsPut)
+  )
+
   private val routes = Seq(
     RegexRoute("^$|^/$".r) { case _ =>
       SPResponse.plainText(
@@ -208,11 +223,8 @@ class SPServer(
           "format is optional, defaults to LabeledData")
     },
     RegexRoute("^/v1/([a-f0-9]{40})$".r("paperId"))(handlePaperId),
-    StringRoute("/v1", "POST")(handlePost),
-    StringRoute("/v1/corrections")(correctionsGetAll),
-    RegexRoute("^/v1/corrections/([a-f0-9]{40})$".r("paperId"), "GET")(correctionsGet),
-    RegexRoute("^/v1/corrections/([a-f0-9]{40})$".r("paperId"), "PUT")(correctionsPut)
-  )
+    StringRoute("/v1", "POST")(handlePost)
+  ) ++ (if(feedbackStore.isDefined) feedbackRoutes else Seq.empty)
 
   override def handle(
     target: String,
@@ -315,12 +327,13 @@ class SPServer(
     SPResponse(200, "application/json", content)
   }
 
-  private val feedbackStore = FeedbackStore // We're triggering this early, so that the FeedbackStore initializes before the first request.
+  private val feedbackUnavailableException =
+    SPServerException(403, "Feedback has been disabled on this server")
 
-  private def correctionsGet(request: SPRequest, regexGroups: Map[String, String]) = {
+  private def correctionsGet(request: SPRequest, regexGroups: Map[String, String]) = feedbackStore.map { store =>
     val paperId = regexGroups("paperId")
     val formatString = request.queryParams.getOrElse("format", "LabeledData")
-    val feedback = feedbackStore.getFeedback(paperId).getOrElse(
+    val feedback = store.getFeedback(paperId).getOrElse(
       throw new SPServerException(404, s"No feedback collected for $paperId"))
     formatString match {
       case "LabeledData" =>
@@ -328,9 +341,9 @@ class SPServer(
       case _ =>
         throw SPServerException(400, s"Could not understand output format '$formatString'.")
     }
-  }
+  }.getOrElse(throw feedbackUnavailableException)
 
-  private def correctionsPut(request: SPRequest, regexGroups: Map[String, String]) = {
+  private def correctionsPut(request: SPRequest, regexGroups: Map[String, String]) = feedbackStore.map { store =>
     if (request.contentType != "application/json")
       throw SPServerException(400, "Content type for PUT must be application/json.")
 
@@ -355,13 +368,13 @@ class SPServer(
     if(!input.id.startsWith("feedback:"))
       input = input.copy(id = s"feedback:${input.id}")
 
-    feedbackStore.addFeedback(paperId, input)
+    store.addFeedback(paperId, input)
 
     SPResponse.Success
-  }
+  }.getOrElse(throw feedbackUnavailableException)
 
-  private def correctionsGetAll(request: SPRequest) = {
-    val result = feedbackStore.getAllFeedback
+  private def correctionsGetAll(request: SPRequest) = feedbackStore.map { store =>
+    val result = store.getAllFeedback
     // This keeps the whole result set in memory, which is bad. It should be streamed.
     // ScalikeJDBC already insists on keeping it in memory, so I didn't take the time to optimize
     // it here.
@@ -370,5 +383,5 @@ class SPServer(
       "application/json",
       result.map(_.toJson.compactPrint).mkString("\n").getBytes("UTF-8")
     )
-  }
+  }.getOrElse(throw feedbackUnavailableException)
 }
