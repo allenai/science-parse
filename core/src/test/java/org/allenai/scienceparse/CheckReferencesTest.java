e0976b69035037f2ecb5f3fc1607f7090ab3ee58
Dirk Groeneveld <dirkg@allenai.org>
2017-05-23 16:42:07 -0700

    Another fix for release-only tests

--- core/src/test/java/org/allenai/scienceparse/CheckReferencesTest.java
+++ core/src/test/java/org/allenai/scienceparse/CheckReferencesTest.java
@@ -13,20 +13,18 @@ import java.util.Arrays;
 public class CheckReferencesTest {
   public void smallTest() throws IOException {
     final String jsonFile =
-            Datastore.apply().filePath("org.allenai.scienceparse", "gazetteer.json", 1).toString();
+            Datastore.apply().filePath("org.allenai.scienceparse", "gazetteer.json", 5).toString();
     CheckReferences cr = new CheckReferences(jsonFile);
     log.info("num hashes: " + cr.getHashSize());
-    Assert.assertEquals(13579, cr.getHashSize());
+    Assert.assertEquals(1557178, cr.getHashSize());
     Assert.assertTrue(cr.hasPaper(
-      "Text-based measures of document diversity",
-      Arrays.asList("Kevin Bache",
-        "David Newman",
-        "Padhraic Smyth"), 2013, "KDD"));
+      "Ecological Sampling of Gaze Shifts",
+      Arrays.asList("Giuseppe Boccignone",
+        "Mario Ferraro"), 2014, "KDD"));
     Assert.assertTrue(cr.hasPaper(
-      "Text-based measures of document diversity",
-      Arrays.asList("K. Bache",
-        "D. Newman",
-        "P. Smyth"), 2013, "KDD"));
+      "HIST: A Methodology for the Automatic Insertion of a Hierarchical Self Test",
+      Arrays.asList("Oliver F. Haberl",
+        "Thomas Kropf"), 1992, "KDD"));
     Assert.assertFalse(cr.hasPaper(
       "Fake paper titles: A case study in negative examples",
       Arrays.asList("Kevin Bache",
